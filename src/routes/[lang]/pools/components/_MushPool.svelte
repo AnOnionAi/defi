9ed3c422d8518ea3040dbbaa31d2fdf1ac34234f
enrique@zeti.software <enrique@zeti.software>
2021-09-07 23:16:14 -0500

    component routes

---
+++ src/routes/[lang]/pools/components/_MushPool.svelte
@@ -0,0 +1,53 @@
+<div id="mush" class="flex justify-center p-5 text-center rounded-md">
+	<br />
+	<div
+		class="mush-w p-2 text-2xl space-y-3 flex flex-col justify-around rounded-md backdrop-filter bg-transparent"
+	>
+		<p class="text-8xl font-bold text-white">MUSH</p>
+		<br />
+		<div class="px-5 bg-gray-50 bg-gradient-to-tl rounded-md">
+			<div class="flex justify-between">
+				<p class="font-black text-black">APY</p>
+				<p class="font-black text-black">999%</p>
+			</div>
+			<div class="flex justify-between">
+				<p class="font-black text-black">APR</p>
+				<p class="font-black text-black">999%</p>
+			</div>
+			<div class="flex justify-between">
+				<p class="font-black text-black">EARN</p>
+				<p class="font-black text-black">MUSH</p>
+			</div>
+			<div class="flex justify-between">
+				<p class="font-black text-black">FEE</p>
+				<p class="font-black text-black">0%</p>
+			</div>
+		</div>
+		<a
+			href="#"
+			class="block bg-green-400 text-white font-bold p-1 rounded-md w-full hover:bg-green-600"
+		>
+			Unlock
+		</a>
+	</div>
+</div>
+
+<style>
+	#mush {
+		background-image: url('../../../../static/mushpan.jpg');
+		width: 70%;
+		height: 400px;
+		margin: 0 auto;
+		background-size: cover;
+	}
+
+	.mush-w {
+		width: 35%;
+	}
+
+	@media (max-width: 800px) {
+		.mush-w {
+			width: 90%;
+		}
+	}
+</style>
