1711d8532a3063248824f6da130a49926b71f660
enrique@zeti.software <enrique@zeti.software>
2021-09-27 11:58:39 -0500

    env variables

---
+++ src/lib/env.ts
@@ -0,0 +1,41 @@
+let env: {};
+
+env = import.meta.env;
+
+const getEnvFiltered = (regex: RegExp, env) => {
+	const newObject = {};
+	for (const [key, value] of Object.entries(env)) {
+		if (key.match(regex)) {
+			const newKey = key.replace(/^VITE_(DEV_|PROD_|QA_)?/, '');
+			newObject[newKey] = value;
+		}
+	}
+	return newObject;
+};
+
+const getEnv = () => {
+	const NODE_ENV = env['VITE_NODE_ENV'];
+
+	console.log('ENV:!!!', NODE_ENV);
+
+	switch (NODE_ENV) {
+		case 'qa':
+			env = getEnvFiltered(/VITE_(?!PROD|DEV)/, env);
+			break;
+
+		case 'production':
+			env = getEnvFiltered(/VITE_(?!DEV|QA)/, env);
+			break;
+
+		default:
+			env = getEnvFiltered(/VITE_(?!PROD|QA)/, env);
+	}
+
+	console.log(env);
+
+	return env;
+};
+
+export default {
+	...getEnv()
+};
