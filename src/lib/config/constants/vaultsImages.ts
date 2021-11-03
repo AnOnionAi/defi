ba5f4d09cf3164922bad5f6930dec02180ffe81a
georgy <georgy@zeti.dev>
2021-11-03 12:19:47 -0600

    VaultFilter added

---
+++ src/lib/config/constants/vaultsImages.ts
@@ -0,0 +1,181 @@
+export const quickImages = [
+	{
+        pair: {
+            token0Name: 'WMATIC',
+			token1Name: 'USDC',
+            token0ImagePath: '/vaultTokensIcons/wmatic.svg',
+			token1ImagePath: '/vaultTokensIcons/usdc.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'WMATIC',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/wmatic.svg'
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'USDT',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/usdt.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WMATIC',
+			token1Name: 'SOL',
+            token0ImagePath: '/vaultTokensIcons/wmatic.svg',
+			token1ImagePath: '/vaultTokensIcons/sol.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'DAI',
+			token1Name: 'USDC',
+            token0ImagePath: '/vaultTokensIcons/dai.svg',
+			token1ImagePath: '/vaultTokensIcons/usdc.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WMATIC',
+			token1Name: 'USDT',
+            token0ImagePath: '/vaultTokensIcons/wmatic.svg',
+			token1ImagePath: '/vaultTokensIcons/usdt.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WMATIC',
+			token1Name: 'QUICK',
+            token0ImagePath: '/vaultTokensIcons/wmatic.svg',
+			token1ImagePath: '/vaultTokensIcons/quick.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'LINK',
+			token1Name: 'WETH',
+            token0ImagePath: '/vaultTokensIcons/link.svg',
+			token1ImagePath: '/vaultTokensIcons/weth.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'QUICK',
+			token1Name: 'CNTR',
+            token0ImagePath: '/vaultTokensIcons/quick.svg',
+			token1ImagePath: '/vaultTokensIcons/cntr.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'DAI',
+			token1Name: 'USDT',
+            oken0ImagePath: '/vaultTokensIcons/dai.svg',
+			token1ImagePath: '/vaultTokensIcons/usdt.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'USDC',
+			token1Name: 'QUICK',
+            token0ImagePath: '/vaultTokensIcons/usdc.svg',
+			token1ImagePath: '/vaultTokensIcons/quick.svg',
+        }
+    },
+];
+
+export const sushiImages = [
+	{
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'USDC',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/usdc.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WBTC',
+			token1Name: 'WETH',
+            token0ImagePath: '/vaultTokensIcons/wbtc.svg',
+			token1ImagePath: '/vaultTokensIcons/weth.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'WMATIC',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/wmatic.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'AAVE',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/aave.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WBTC',
+			token1Name: 'ibBTC',
+            token0ImagePath: '/vaultTokensIcons/wbtc.svg',
+			token1ImagePath: '/vaultTokensIcons/ibbtc.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'LINK',
+			token1Name: 'WETH',
+            token0ImagePath: '/vaultTokensIcons/link.svg',
+			token1ImagePath: '/vaultTokensIcons/weth.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'GRT',
+			token1Name: 'WETH',
+            token0ImagePath: '/vaultTokensIcons/grt.svg',
+			token1ImagePath: '/vaultTokensIcons/weth.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'DAI',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/dai.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'AXS',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/axs.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'CRV',
+			token1Name: 'WETH',
+            token0ImagePath: '/vaultTokensIcons/crv.svg',
+			token1ImagePath: '/vaultTokensIcons/weth.svg',
+        }
+    },
+    {
+        pair: {
+            token0Name: 'WETH',
+			token1Name: 'AVAX',
+            token0ImagePath: '/vaultTokensIcons/weth.svg',
+			token1ImagePath: '/vaultTokensIcons/avax.svg',
+        }
+    }
+]
