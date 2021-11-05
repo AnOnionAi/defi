ac021ea12319213f5330882bb89b87088e2d2220
M1 Zeti <enrique@zeti.software>
2021-11-05 15:05:42 -0600

    Fixed pools images from imports to src attr

--- src/lib/config/constants/pools.ts
+++ src/lib/config/constants/pools.ts
@@ -8,74 +8,86 @@ export let pools: PoolInfo[] = [
 		tokenName: 'MUSH',
 		tokenAddr: getContractAddress(Token.MUSHTOKEN),
 		depositFee: 0,
-		pid: 5
+		pid: 5,
+		tokenImagePath: "/static/mushRound.png"
 	},
 	{
 		tokenName: 'FISH',
 		tokenAddr: getContractAddress(Token.FISHTOKEN),
 		depositFee: 0,
-		pid: 4
+		pid: 4,
+		tokenImagePath: "/static/vaultTokensIcons/fish.svg"
 	},
 	{
 		tokenName: 'WMATIC',
 		tokenAddr: getContractAddress(Token.WMATIC),
 		depositFee: 0,
-		pid: 4
+		pid: 4,
+		tokenImagePath: "/static/vaultTokensIcons/wmatic.svg"
 	},
 	{
 		tokenName: 'WETH',
 		tokenAddr: getContractAddress(Token.WETH),
 		depositFee: 0,
-		pid: 4
+		pid: 4,
+		tokenImagePath: "/static/vaultTokensIcons/weth.svg"
 	},
 	{
 		tokenName: 'WBTC',
 		tokenAddr: getContractAddress(Token.WBTC),
 		depositFee: 0,
-		pid: 6
+		pid: 6,
+		tokenImagePath: "/static/vaultTokensIcons/wbtc.svg"
 	},
 	{
 		tokenName: 'SUSHI',
 		tokenAddr: getContractAddress(Token.SUSHI),
 		depositFee: 0,
-		pid: 7
+		pid: 7,
+		tokenImagePath: "/static/sushi.png"
 	},
 	{
 		tokenName: 'QUICK',
 		tokenAddr: getContractAddress(Token.QUICK),
 		depositFee: 0,
-		pid: 8
+		pid: 8,
+		tokenImagePath: "/static/vaultTokensIcons/quick.svg"
 	},
 	{
-		tokenName: 'DYFN',
+		tokenName: 'DFYN',
 		tokenAddr: getContractAddress(Token.DFYN),
 		depositFee: 0,
-		pid: 9
+		pid: 9,
+		tokenImagePath: "/static/dfyn.svg"
 	},
 	{
 		tokenName: 'USDT',
 		tokenAddr: getContractAddress(Token.USDT),
 		depositFee: 0,
-		pid: 10
+		pid: 10,
+		tokenImagePath: "/static/vaultTokensIcons/usdt.svg"
 	},
 	{
 		tokenName: 'USDC',
 		tokenAddr: getContractAddress(Token.USDC),
 		depositFee: 0,
-		pid: 11
+		pid: 11,
+		tokenImagePath: "/static/vaultTokensIcons/usdc.svg"
 	},
 
 	{
 		tokenName: 'DOGE',
 		tokenAddr: getContractAddress(Token.DOGE),
 		depositFee: 0,
-		pid: 12
+		pid: 12,
+		tokenImagePath: "/static/doge.png"
 	},
 
 	{
 		tokenName: 'MXNC',
 		tokenAddr: getContractAddress(Token.MXNC),
 		depositFee: 0,
-		pid: 13
+		pid: 13,
+		tokenImagePath: "/static/mxnc.png"
 	}
 ];
