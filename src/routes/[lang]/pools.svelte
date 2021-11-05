083ea8da93b477cdecb2c161b7fd5ac2b7cda75d
M1 Zeti <enrique@zeti.software>
2021-11-05 15:43:40 -0600

    removed unusual imports

--- src/routes/[lang]/pools.svelte
+++ src/routes/[lang]/pools.svelte
@@ -11,18 +11,6 @@
 </script>
 
 <script lang="ts">
-	import doge from '/static/doge.png';
-	import mxnc from '/static/mxnc.png';
-	import sushi from '/static/sushi.png';
-	import fish from '/static/fish.svg';
-	import dyfn from '/static/dfyn.svg';
-	import quick from '/static/quiswa.png';
-	import matic from '/static/matic.png';
-	import weth from '/static/weth.png';
-	import wbtc from '/static/wbtc.png';
-	import usdt from '/static/usdt.png';
-	import usdc from '/static/usdc.png';
-	import mushRound from '/static/mushRound.png';
 	import PoolCard from '$lib/components/Cards/PoolCard.svelte';
 	import MushCard from '$lib/components/Cards/MushPool.svelte';
 	import { pools } from '$lib/config/constants/pools';
@@ -35,7 +23,7 @@
 		<br />
 		<h1 class="text-dark-200 dark:text-white text-4xl">P O O L S</h1>
 		<div class="mt-5 space-y-4">
-			<div class="flex flex-row justify-center gap-y-9 gap-x-4 p-8 text-center flex-wrap">
+			<div class="flex flex-row justify-center gap-y-9 gap-x-4 p-8 text-center flex-wrap max-w-6xl mx-auto">
 				{#each pools as pool  }
 				<PoolCard  info={pool} />
 				{/each}
