724d93dace77605097d202bfcd5f97fa34badcaa
M1 Zeti <enrique@zeti.software>
2021-11-17 12:06:50 -0600

    three minifier added, but isn't working lmao

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from 'svelte-preprocess';
 import netlify from '@sveltejs/adapter-netlify';
 import * as WindiCSS from 'vite-plugin-windicss';
+import { threeMinifier } from "@yushijinhun/three-minifier-rollup";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,7 +13,7 @@ const config = {
 		adapter: netlify(),
 		target: '#svelte',
 		vite: {
-			plugins: [WindiCSS.default()]
+			plugins: [WindiCSS.default(),threeMinifier()]
 		}
 	}
 };
