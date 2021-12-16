6f3c54c34ef35e984172a80e9d91f9e5efd21327
Matthew <matthew@matthew.com>
2021-12-16 09:49:59 -0600

    windi is working

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,7 @@ import WindiCSS from 'vite-plugin-windicss';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess()],
-
+	preprocess: preprocess(),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: netlify(),
