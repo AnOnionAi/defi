a26417189fb2d9797528b5fff1b1a15d63e5d230
Enrique Castillo <enriquecastillo@MacBook-Pro-de-Enrique.local>
2022-04-08 17:59:56 -0500

    homescreen visual finished

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -44,15 +44,14 @@
 </script>
 
 <section
-	class="relative flex  h-full select-none flex-col bg-paper dark:bg-darkGrey-900    ">
-	<div
-		class=" top-0 w-full bg-primary-400 py-4 px-4 dark:bg-analogPurple-300 md:absolute">
+	class="flex h-full select-none  flex-col bg-paper transition duration-500 dark:bg-darkGrey-900    ">
+	<div class=" w-full bg-primary-400 py-4 px-6 dark:bg-analogPurple-300 ">
 		<h2
 			class=" hidden text-center text-xl font-semibold text-white md:block md:text-2xl">
 			TIME LEFT FOR OFFICIAL LAUNCH:
 			{#if timeleft?.days}
 				{timeleft.days} DAYS {timeleft.hours} HOURS
-				{timeleft.minutes} MINUTES {timeleft.seconds} SECONDS
+				{timeleft.minutes} MINUTES {timeleft.seconds} SECONDS 🚀
 			{/if}
 		</h2>
 
@@ -66,93 +65,149 @@
 		</h2>
 	</div>
 	<div
-		class=" flex h-full flex-col items-center justify-center py-8 text-neutral-900 dark:text-white">
-		<div class="mb-12 flex flex-col items-center">
+		class=" flex flex-1 flex-col items-center justify-center py-8 text-gray-900 dark:text-white">
+		<div class="mb-10 flex flex-col items-center">
 			<img
 				src="/theme/fiji.svg"
 				alt="Fiji the Fung"
 				class="w-16 md:w-20 lg:w-24 xl:w-28 " />
 			<h1
-				class="bg-analogMelon-200 bg-gradient-to-t from-primary-400 bg-clip-text p-3 text-5xl font-bold text-transparent dark:bg-gradient-to-b dark:from-triadicYellow-400 dark:to-analogPurple-300 md:text-6xl lg:text-7xl  xl:text-8xl">
+				class="bg-analogMelon-200 bg-gradient-to-t from-primary-400 bg-clip-text p-3 text-4xl font-bold text-transparent dark:bg-gradient-to-b dark:from-triadicYellow-400 dark:to-analogPurple-300 md:text-6xl lg:text-7xl  xl:text-8xl">
 				Fung Finance
 			</h1>
 			<h2 class="text-xl font-semibold md:text-2xl lg:text-3xl">
-				Make it Mushcoin
+				Make It Mushcoin!
 			</h2>
 		</div>
-		<div class="">
+		<div class="px-6">
+			<p
+				class="text-center text-lg font-medium text-gray-600 dark:text-gray-200 md:text-left">
+				Presents
+			</p>
 			<h3 class="text-center text-2xl font-bold md:text-3xl lg:text-4xl">
-				Presenting
+				The Revolution of Decentralized Finance
 			</h3>
-			<div class="mt-8 flex flex-col gap-3 py-2 md:flex-row md:gap-6">
-				<div class="group">
-					<div
-						class="flex min-h-[232px] max-w-[260px] cursor-pointer flex-col items-center gap-4 rounded-xl border-2 bg-white px-4 py-5  group-hover:border-primary-300 dark:border-neutral-600 dark:bg-neutral-800 dark:group-hover:border-analogPurple-300">
-						<img
-							src="/assets/mushCoin.webp"
-							alt="The Mushcoin"
-							class="h-[36px] w-[36px]" />
+		</div>
 
-						<div>
-							<h4
-								class="mb-1 text-center text-sm font-medium group-hover:text-primary-400 dark:group-hover:text-analogPurple-300">
-								Mush Coin
-							</h4>
-							<p
-								class="text-xs  group-hover:text-primary-300 dark:group-hover:text-analogPurple-300 ">
-								MushCoin is how people express their stake in FungFi’s future.
-								FungFi is owned by MUSH holders and all fees collected by our
-								yield automation products will be distributed through our
-								dividend pool from day one.
-							</p>
+		<div class="overflow-hidden  py-2">
+			<div class="container m-auto space-y-2 px-8 text-gray-500 md:px-12">
+				<div
+					class="mt-4    grid divide-x divide-y overflow-hidden rounded-xl border dark:divide-neutral-700 dark:border-neutral-700 sm:grid-cols-2 lg:grid-cols-3 lg:divide-y-0 xl:grid-cols-4">
+					<div
+						class="group relative bg-white transition hover:z-[1] hover:shadow-2xl dark:bg-neutral-900">
+						<div class="relative space-y-8 p-8">
+							<img
+								src="/assets/mushcoin.webp"
+								class="w-10"
+								width="512"
+								height="512"
+								alt="burger illustration" />
+
+							<div class="space-y-2">
+								<h5
+									class="text-xl font-medium text-gray-800 transition group-hover:text-primary-400 dark:text-gray-100 dark:group-hover:text-analogPurple-300">
+									Mush Coin
+								</h5>
+								<p class="text-sm  text-gray-600 dark:text-gray-200">
+									MushCoin is how people express their stake in FungFi’s future.
+									MUSH will be distributed by staking in pools and farms.
+								</p>
+							</div>
+							<a
+								href="#"
+								class="flex items-center justify-between group-hover:text-primary-400 dark:text-gray-200 dark:group-hover:text-analogPurple-300">
+								<span
+									class="-translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+									>&RightArrow;</span>
+							</a>
 						</div>
 					</div>
-				</div>
-
-				<div class="group">
 					<div
-						class="flex min-h-[232px] max-w-[260px] cursor-pointer flex-col items-center gap-4 rounded-xl border-2 bg-white px-4 py-5  group-hover:border-primary-300 dark:border-neutral-600 dark:bg-neutral-800 dark:group-hover:border-analogPurple-300">
-						<img
-							src="/assets/mushroomFarms.svg"
-							alt="The Mushcoin"
-							class="h-[36px] w-[36px]" />
+						class="group relative bg-white transition hover:z-[1] hover:shadow-2xl dark:bg-neutral-900">
+						<div class="relative space-y-8 p-8">
+							<img
+								src="/assets/mushroomFarms.svg"
+								class="w-10"
+								width="512"
+								height="512"
+								alt="burger illustration" />
 
-						<div>
-							<h4
-								class="mb-1 text-center text-sm font-medium group-hover:text-primary-400 dark:group-hover:text-analogPurple-300">
-								Mushroom Farms
-							</h4>
-							<p
-								class="text-xs group-hover:text-primary-300 dark:group-hover:text-analogPurple-300 ">
-								Customers who stake MUSH/USDC LP tokens in FungFi farms will be
-								rewarded with MushCoin during the token distribution period.
-								there are no deposit or withdrawal fees for staking MUSH/USDC LP
-								tokens. We incentivize liquidity on Sushiswap.
-							</p>
+							<div class="space-y-2">
+								<h5
+									class="text-xl font-medium text-gray-800 transition group-hover:text-primary-400 dark:text-gray-100 dark:group-hover:text-analogPurple-300">
+									Farms
+								</h5>
+								<p class="text-sm text-gray-600 dark:text-gray-200">
+									Customers who stake MUSH/USDC LP tokens in FungFi farms will
+									be rewarded with MushCoin.
+								</p>
+							</div>
+							<a
+								href="#"
+								class="flex items-center justify-between group-hover:text-primary-400  dark:text-gray-200 dark:group-hover:text-analogPurple-300">
+								<span
+									class="-translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+									>&RightArrow;</span>
+							</a>
 						</div>
 					</div>
-				</div>
-
-				<div class="group">
 					<div
-						class="flex min-h-[232px]  max-w-[260px] cursor-pointer flex-col items-center gap-4 rounded-xl border-2 bg-white px-4 py-5  group-hover:border-primary-300 dark:border-neutral-600 dark:bg-neutral-800 dark:group-hover:border-analogPurple-300">
-						<img
-							src="/assets/cryptoVault.svg"
-							alt="The Mushcoin"
-							class="h-[36px] w-[36px]" />
+						class="group relative bg-white transition hover:z-[1] hover:shadow-2xl dark:bg-neutral-900">
+						<div class="relative space-y-8 p-8">
+							<img
+								src="/assets/pool.svg"
+								class="w-10"
+								width="512"
+								height="512"
+								alt="burger illustration" />
 
-						<div>
-							<h4
-								class="mb-1 text-center text-sm font-medium group-hover:text-primary-400 dark:group-hover:text-analogPurple-300">
-								FungFi Vaults
-							</h4>
-							<p
-								class="text-xs  group-hover:text-primary-300 dark:group-hover:text-analogPurple-300 ">
-								Allow users to automatically compound farming tokens back into
-								the assets they originally staked. We’ll be launching vaults for
-								a a number of different platforms, including Sushiswap. &
-								Quickswap.
-							</p>
+							<div class="space-y-2">
+								<h5
+									class="text-xl font-medium text-gray-800 transition group-hover:text-primary-400 dark:text-gray-100 dark:group-hover:text-analogPurple-300">
+									Pools
+								</h5>
+								<p class="text-sm text-gray-600 dark:text-gray-200">
+									FungFi users will be able to stake other tokens into our pools
+									to start earning $MUSH every second.
+								</p>
+							</div>
+							<a
+								href="#"
+								class="flex items-center justify-between group-hover:text-primary-400 dark:text-gray-200 dark:group-hover:text-analogPurple-300">
+								<span
+									class="-translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+									>&RightArrow;</span>
+							</a>
+						</div>
+					</div>
+					<div
+						class="group relative bg-gray-100 transition hover:z-[1] hover:shadow-2xl dark:bg-neutral-900 lg:hidden xl:block">
+						<div
+							class="relative space-y-8 rounded-lg border-dashed p-8 transition duration-300 group-hover:scale-90 group-hover:border group-hover:bg-white dark:group-hover:bg-neutral-800">
+							<img
+								src="/assets/vaultExchange.svg"
+								class="w-10"
+								width="512"
+								height="512"
+								alt="burger illustration" />
+
+							<div class="space-y-2">
+								<h5
+									class="text-xl font-medium text-gray-800 transition group-hover:text-primary-400 dark:text-gray-100 dark:group-hover:text-analogPurple-300">
+									FungFi Vaults
+								</h5>
+								<p class="text-sm text-gray-600 dark:text-gray-200">
+									Allow users to automatically compound farming tokens back into
+									the assets they originally staked.
+								</p>
+							</div>
+							<a
+								href="#"
+								class="flex items-center justify-between group-hover:text-primary-400 dark:text-gray-200 dark:group-hover:text-analogPurple-300">
+								<span
+									class="-translate-x-4 text-2xl opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100"
+									>&RightArrow;</span>
+							</a>
 						</div>
 					</div>
 				</div>
