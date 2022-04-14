6d3ccc5cea767bb8e7a4512b1fec40aedcd1423c
Enrique Castillo <enriquecastillo@MacBook-Pro-de-Enrique.local>
2022-04-13 23:58:36 -0500

    spanish translation for home and new components

--- src/lib/components/Cards/DepositWarning.svelte
+++ src/lib/components/Cards/DepositWarning.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { launchDate, timeLeftForLaunch } from '$lib/stores/launchDate';
+	import { _ } from 'svelte-i18n';
 </script>
 
 <div
-	class="bg-primary-300 py-2 px-4 text-center font-semibold text-gray-200 dark:bg-analogPurple-300">
-	⚠️ Deposits and Withdrawals are available but you will not earn MUSH until {$timeLeftForLaunch.daysLeft}
+	class="select-none bg-primary-300 py-2 px-4 text-center font-semibold text-gray-200 dark:bg-analogPurple-300">
+	⚠️ {$_('depositWarning.warning')}{$timeLeftForLaunch.daysLeft}
 	Days
 	{$timeLeftForLaunch.hoursLeft} Hours {$timeLeftForLaunch.minutesLeft} Minutes {$timeLeftForLaunch.secondsLeft}
 	Seconds ⚠️
