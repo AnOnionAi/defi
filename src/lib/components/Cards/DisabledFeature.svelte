4c4503347998567467c6dadf870b2cd952264c7b
Enrique Castillo <enriquecastillo@MacBook-Pro-de-Enrique.local>
2022-04-28 12:34:11 -0500

    changed heading text to coming soon

--- src/lib/components/Cards/DisabledFeature.svelte
+++ src/lib/components/Cards/DisabledFeature.svelte
@@ -3,7 +3,7 @@
 
 <div class="mt-[100px] select-none px-4 text-center md:mt-[200px]">
 	<h2 class="text-3xl font-bold text-gray-800 dark:text-gray-200 md:text-4xl">
-		Feature Disabled
+		Coming Soon
 	</h2>
 	<p class="mt-2 text-xl text-gray-600 dark:text-gray-300 md:text-2xl">
 		Developers are testing this feature. Thanks for your patience
