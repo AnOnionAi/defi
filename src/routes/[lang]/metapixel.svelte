cde62b90e4d36154773e19e7fe956ea77d4f9f8f
georgy <georgy@zeti.dev>
2022-05-06 13:29:58 -0500

    metapixel updated

--- src/routes/[lang]/metapixel.svelte
+++ src/routes/[lang]/metapixel.svelte
@@ -1,24 +1,38 @@
 <script lang="ts" module="context">
 	import { onMount } from 'svelte';
-	import '@melloware/coloris/dist/coloris.css';
-	import Coloris from '@melloware/coloris';
 	import DisabledFeature from '$lib/components/Cards/DisabledFeature.svelte';
 
-	const development = true;
-
-	let pixelsData = [];
+	const development = false;
 
 	let gridContainer;
 	let size = 64;
 
+	let inputColor;
+
+	const changeColor = (e) => {
+		e.target.style.backgroundColor = inputColor.value;
+	};
+
 	onMount(() => {
-		Coloris.init();
+		const grid = document.querySelector('#grid');
 
-		Coloris({ el: '.pixel', defaultColor: '#000000' });
-		Coloris.close();
+		for (let i = 0; i < 64; i++) {
+			for (let j = 0; j < 64; j++) {
+				const div = document.createElement('div');
+				div.classList.add('pixel');
+				div.style.backgroundColor = '#f9fafb';
+				grid.appendChild(div);
 
-		for (let index = 0; index < size * size; index++) {
-			pixelsData[index] = '#ffffff';
+				div.addEventListener('click', changeColor);
+				div.addEventListener('mouseover', () => {
+					div.style.transform = 'scale(2)';
+					div.style.zIndex = '2';
+				});
+				div.addEventListener('mouseleave', () => {
+					div.style.transform = 'scale(1)';
+					div.style.zIndex = '0';
+				});
+			}
 		}
 	});
 </script>
@@ -26,60 +40,57 @@
 {#if development}
 	<DisabledFeature />
 {:else}
-	<div bind:this={gridContainer} id="grid" class="m-auto mb-8 mt-8 w-11/12">
-		{#each pixelsData as pixel}
-			<div class="clr-field" style="color: {pixel};">
+	<div class="Metapixel grid grid-cols-2">
+		<div class="information p-4">
+			<div
+				class="options bg-white dark:bg-neutral-800 grid grid-cols-2 w-max m-auto rounded-2xl sideShadow">
+				<input bind:this={inputColor} type="color" id="color" value="#fe7688" />
 				<button
-					aria-labelledby="clr-open-label"
-					style="width: 100%; height:100%;" />
-				<input
-					class="pixel"
-					type="text"
-					style="width: 100%;
-				height: max-content;
-				border-radius: 2px;" />
+					class="flex items-center bg-black disabled:opacity-50 false text-white font-semibold rounded-lg px-5 py-3 tracking-wide hover:bg-pink-500 s-YQIdR16N_soy"
+					data-dashlane-rid="96e3e72566535f11"
+					data-dashlane-label="true"
+					data-form-type="action"
+					><p>Paint</p>
+				</button>
 			</div>
-		{/each}
+			<div class="description mt-8">
+				<h1 class="dark:text-white">Jackpot:</h1>
+				<h1 class="dark:text-white">Token:</h1>
+				<h1 class="dark:text-white">Price to Paint:</h1>
+			</div>
+		</div>
+
+		<div bind:this={gridContainer} id="grid" class="m-auto mb-8 mt-2 w-11/12" />
 	</div>
 {/if}
 
 <style>
+	.Metapixel {
+		grid-template-columns: 20% 80%;
+	}
+
 	#grid {
-		background-color: rgb(40, 40, 40);
+		background-color: #374151;
+	}
+
+	.options {
+		padding: 1em;
+		margin-bottom: 1em;
+		justify-content: center;
+		align-items: center;
+	}
+
+	#grid {
+		height: 90vh;
+		width: 90vh;
 		display: grid;
 		grid-template-columns: repeat(64, 1fr);
 		grid-template-rows: repeat(64, 1fr);
-		gap: 3px;
-		padding: 3px;
+		gap: 1px;
+		padding: 1px;
 	}
 
-	.clr-field {
-		display: inline-block;
-		position: relative;
-	}
-
-	.pixel {
-		width: 5px;
-		height: 5px;
-		padding: 0 10px;
-		border: 1px solid #ccc;
-		border-radius: 5px;
-		font-family: inherit;
-		font-size: inherit;
-		font-weight: inherit;
-		box-sizing: border-box;
-	}
-
-	.clr-field button:after {
-		content: '';
-		display: block;
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		left: 0;
-		top: 0;
-		border-radius: inherit;
-		background-color: currentColor;
-		box-shadow: inset 0 0 1px rgb(0 0 0 / 50%);
+	.sideShadow {
+		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 	}
 </style>
